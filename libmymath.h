2ec49a760eaac231fdc7436a95be988f33dd9348
TranHuuBinh <tranhuubinh17071997@gmail.com>
2018-05-22 04:33:49 -0700

    My Math

---
+++ libmymath.h
@@ -0,0 +1,5 @@
+double GiaiThua(int n);
+long TinhTongChan(int n);
+long TinhTongLe(int n);
+long LuyThua(int x,int n);
+
---
+++ test-mymath.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Giai thua 100: %.0f \n",GiaiThua(100));
+	printf("Tong so chan den 100: %ld \n",TinhTongChan(100));
+	printf("Tong so le den 100: %ld \n",TinhTongLe(100));
+	printf("8 luy thua 4: %ld \n",LuyThua(8,4));
+	return 0;
+}
